4d6798200e457965d940602b0a8bea4be374170f
Thiago Duarte <106554492+ThiagoDuart4@users.noreply.github.com>
2023-01-13 17:20:58 -0300

    scrollReveal e concertando layout liquido

--- js/batak.js
+++ js/batak.js
@@ -1,33 +1,13 @@
 
-/* SLIDE */
+window.sr=ScrollReveal({ reset:true})
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
+sr.reveal(' .card',{duration:1500})
+sr.reveal(' .card-localization',{duration:1500})
+sr.reveal(' .card-equipe',{duration:1500})
+sr.reveal(' .quemSomos-Left',{duration:1500})
+sr.reveal(' .quemSomos-Rigth',{duration:1500})
+sr.reveal(' .quemSomos-Rigth',{duration:1500})
+sr.reveal(' .contato-info',{duration:1500})
 
 // menu lateral
 
